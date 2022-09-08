c7f85d0b66398f22a8488d7adc5dff07f678e693
Samuel Hym <samuel.hym@rustyne.lautre.net>
2022-09-08 17:24:23 +0200

    Simplify the example where a thread wants to join its own domain

--- src/threadomain/example.ml
+++ src/threadomain/example.ml
@@ -6,32 +6,23 @@ type handle =
   | DomainHdl of unit Domain.t
   | ThreadHdl of Thread.t
 
-let hdls = Array.make 3 NoHdl
-let sems = Array.init 3 (fun _ -> Semaphore.Binary.make false)
+let hdls = Array.make 2 NoHdl
+let sems = Array.init 2 (fun _ -> Semaphore.Binary.make false)
 
 let thd1_main () =
-  ()
-
-let thd2_main () =
   Semaphore.Binary.acquire sems.(0) ;
   ( match hdls.(0) with
     | DomainHdl h -> Domain.join h
-    | _ -> failwith "Unexpected" ) ;
-  Semaphore.Binary.acquire sems.(1) ;
-  ( match hdls.(0) with
-    | ThreadHdl h -> Thread.join h
     | _ -> failwith "Unexpected" )
 
 let dom0_main () =
   hdls.(1) <- ThreadHdl (Thread.create thd1_main ()) ;
-  Semaphore.Binary.release sems.(1) ;
-  hdls.(2) <- ThreadHdl (Thread.create thd2_main ()) ;
-  Semaphore.Binary.release sems.(2)
+  Semaphore.Binary.release sems.(1)
 
 let _ =
   hdls.(0) <- DomainHdl (Domain.spawn dom0_main) ;
   Semaphore.Binary.release sems.(0) ;
-  Semaphore.Binary.acquire sems.(2) ;
-  ( match hdls.(2) with
+  Semaphore.Binary.acquire sems.(1) ;
+  ( match hdls.(1) with
     | ThreadHdl h -> Thread.join h
     | _ -> failwith "Unexpected" )
