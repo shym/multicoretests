59be3c1c3cf25035a54cf0969faf569c714e9ab7
Samuel Hym <samuel.hym@rustyne.lautre.net>
2022-11-21 09:45:33 +0100

    [CI] WIP

--- src/runner.sh
+++ src/runner.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # A runner for tests that adds timestamps and, in CI, anchors in logs
 test="$1"
-printf '\n\nStarting "./%s --verbose" at %(%c)T\n' "$test" -1
+printf '\n\nStarting "./%s --verbose" at ' "$test"
+date
 if [ "$CI" = true ] ; then
   WARN_PREFIX="::warning::"
   ERR_PREFIX="::error::"
@@ -12,26 +13,27 @@ fi
 #
 ./"$test" --verbose
 result="$?"
-if [ "$result" = 1 ] ; then
-  echo "${WARN_PREFIX}Failure in test $test"
-else if [ "$result" '!=' 0 ] ; then
-  case "$result" in
-    139)
-      echo "${ERR_PREFIX}SIGSEGV in test $test"
-      kill -SIGSEGV $$
-      ;;
-    135)
-      echo "${ERR_PREFIX}SIGBUS in test $test"
-      kill -SIGBUS $$
-      ;;
-    137)
-      echo "${ERR_PREFIX}SIGKILL in test $test"
-      kill -SIGKILL $$
-      ;;
-    *)
-      echo "${ERR_PREFIX}Result $result in test $test"
-      ;;
-  esac
-fi
-fi
-exit "$result"
+case "$result" in
+  0) # Success
+    exit 0
+    ;;
+  1) #
+    echo "${WARN_PREFIX}Failure in test $test"
+    exit 1
+  139)
+    echo "${ERR_PREFIX}SIGSEGV in test $test"
+    kill -SIGSEGV $$
+    ;;
+  135)
+    echo "${ERR_PREFIX}SIGBUS in test $test"
+    kill -SIGBUS $$
+    ;;
+  137)
+    echo "${ERR_PREFIX}SIGKILL in test $test"
+    kill -SIGKILL $$
+    ;;
+  *)
+    echo "${ERR_PREFIX}Result $result in test $test"
+    exit "$result"
+    ;;
+esac
