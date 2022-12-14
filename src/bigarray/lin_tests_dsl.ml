402af2c9ed5ec77c2f249cdf16f551c717beb1a1
Samuel Hym <samuel.hym@rustyne.lautre.net>
2022-12-14 14:17:57 +0100

    Add all the functions of Bigarray.Array1 that are not tested yet, and why

--- src/bigarray/lin_tests_dsl.ml
+++ src/bigarray/lin_tests_dsl.ml
@@ -18,10 +18,22 @@ struct
   let int = int_small
 
   let api =
-    [ val_ "Bigarray.Array1.size_in_bytes" Array1.size_in_bytes (t @-> returning int);
+    [ (* Only one t is tested, so skip functions that create arrays or
+         take multiple arrays as input *)
+      (* init *)
+      val_ "Bigarray.Array1.dim"           Array1.dim           (t @-> returning int);
+      (* kind and layout types would require specific configurations *)
+      (* kind *)
+      (* layout *)
+      (* change_layout *)
+      val_ "Bigarray.Array1.size_in_bytes" Array1.size_in_bytes (t @-> returning int);
       val_ "Bigarray.Array1.get"           Array1.get           (t @-> int @-> returning_or_exc int);
       val_ "Bigarray.Array1.set"           Array1.set           (t @-> int @-> int @-> returning_or_exc unit);
+      (* sub *)
+      (* slice (generate an Array0) *)
+      (* blit *)
       val_ "Bigarray.Array1.fill"          Array1.fill          (t @-> int @-> returning unit);
+      (* of_array *)
     ]
 end
 
