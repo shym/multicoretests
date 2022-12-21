c4c2bbc98f5305ec3823e5c992b8a6f14adbfffd
Samuel Hym <samuel.hym@rustyne.lautre.net>
2022-12-21 18:23:31 +0100

    Try range 61-100

--- src/threadomain/threadomain.ml
+++ src/threadomain/threadomain.ml
@@ -163,7 +163,7 @@ let run_all_nodes sj =
 let main_test = Test.make ~name:"Mash up of threads and domains"
                           ~count:500
                           ~print:show_spawn_join
-                          (Gen.sized_size (Gen.int_range 31 60) gen_spawn_join)
+                          (Gen.sized_size (Gen.int_range 61 100) gen_spawn_join)
                           run_all_nodes
                           (* to debug deadlocks: *)
                           (* (Util.fork_prop_with_timeout 1 run_all_nodes) *)
