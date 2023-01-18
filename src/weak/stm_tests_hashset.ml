fd53bb1712e88d6345dfb181b85b4018868e979d
Jan Midtgaard <mail@janmidtgaard.dk>
2023-01-18 17:39:47 +0100

    quickfix: increase neg_test count to 1500

--- src/weak/stm_tests_hashset.ml
+++ src/weak/stm_tests_hashset.ml
@@ -145,5 +145,5 @@ module WeakHashsetSTM_dom = STM_domain.Make(WHSConf)
 ;;
 QCheck_base_runner.run_tests_main
   [ WeakHashsetSTM_seq.agree_test         ~count:1000 ~name:"STM Weak HashSet test sequential";
-    WeakHashsetSTM_dom.neg_agree_test_par ~count:1000 ~name:"STM Weak HashSet test parallel";
+    WeakHashsetSTM_dom.neg_agree_test_par ~count:1500 ~name:"STM Weak HashSet test parallel";
   ]
