7669f00233ff66b7c2b965e7ff5e8a169267b067
Samuel Hym <samuel.hym@rustyne.lautre.net>
2023-03-09 19:30:12 +0100

    Simplify a bit the tester

--- BufferStress.ml
+++ BufferStress.ml
@@ -2,26 +2,23 @@ let buf = Buffer.create 16
 
 let tester () =
   Buffer.reset buf;
-  Buffer.add_string buf "l;]\184\240\178}\244\222\225\251yG\174";
+  Buffer.add_string buf "aaaaaaaaaaaaaa";
+
   let wait = Atomic.make true in
   let dom1 =
     Domain.spawn (fun () ->
-        while Atomic.get wait do
-          Domain.cpu_relax ()
-        done;
-        Buffer.clear buf;
+        while Atomic.get wait do Domain.cpu_relax () done;
         Buffer.clear buf)
   in
   let dom2 =
     Domain.spawn (fun () ->
         Atomic.set wait false;
-        Buffer.add_string buf "3\163\015U")
+        Buffer.add_string buf "aaaa")
   in
-  let obs1 = Domain.join dom1 in
-  let obs2 = Domain.join dom2 in
-  (obs1, obs2)
+  Domain.join dom1;
+  Domain.join dom2
 
 let _ =
-  for _i = 0 to 1000 do
-    tester () |> ignore
+  for _i = 0 to 10 do
+    tester ()
   done
