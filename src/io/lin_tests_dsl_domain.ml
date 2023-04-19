21a0cdcb2fae0cd2f34cc6eb81895e680d3b5f64
Samuel Hym <samuel.hym@rustyne.lautre.net>
2023-04-19 14:13:49 +0200

    Use the general logging

--- src/io/lin_tests_dsl_domain.ml
+++ src/io/lin_tests_dsl_domain.ml
@@ -8,7 +8,17 @@ module IC_domain = Lin_domain.Make(ICConf)
 module OC_domain = Lin_domain.Make(OCConf)
 
 let _ =
-  QCheck_base_runner.run_tests_main [
-    IC_domain.neg_lin_test ~count:1000 ~name:"Lin DSL In_channel test with Domain";
+  Util.set_log_triple_channels (Some (open_out "inputs.log"))
+
+let tests =
+  IC_domain.neg_lin_test ~count:1000 ~name:"Lin DSL In_channel test with Domain" ::
+  if Sys.getenv_opt "OCAML_SYSTEM" = Some "macosx"
+  then (
+    Printf.printf "Lin DSL Out_channel test with Domain disabled under macOS\n\n%!";
+    []
+  ) else [
     OC_domain.neg_lin_test ~count:1000 ~name:"Lin DSL Out_channel test with Domain";
   ]
+
+let _ =
+  QCheck_base_runner.run_tests_main tests
