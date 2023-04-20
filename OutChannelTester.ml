9806ca6dea61c3e475c798d5acada38d59974725
Samuel Hym <samuel.hym@rustyne.lautre.net>
2023-04-20 15:56:03 +0200

    Update template to rerun log

--- OutChannelTester.ml
+++ OutChannelTester.ml
@@ -1,12 +1,22 @@
-let i1 f x = try f x |> ignore with _ -> ()
-let i2 f w x = i1 (f w) x
-let i3 f v w x = i1 (f v w) x
-let i4 f u v w x = i1 (f u v w) x
+module Out_channel = struct
+  include Out_channel
 
-let tester () =
+  let seek ch v = seek ch (Int64.of_int v)
+end
+
+let log =
+  List.map
+    (fun (tk0, tk1, tk2) ->
+      (Array.of_list tk0, Array.of_list tk1, Array.of_list tk2))
+    [
+      (* insert log here *)
+    ]
+
+let tester (tk0, tk1, tk2) =
   let path, t = Filename.open_temp_file "out-channel-" "" in
+  let run thunk = try thunk t with _ -> () in
 
-  i1 Out_channel.close t;
+  Array.iter run tk0;
 
   let wait = Atomic.make true in
   let dom1 =
@@ -14,32 +24,13 @@ let tester () =
         while Atomic.get wait do
           Domain.cpu_relax ()
         done;
-
-        i1 Out_channel.pos t;
-        i4 Out_channel.output_substring t
-          "\1707NW\000'an\191\015\145\n\
-          \ \
-           \236C\250\027\200\185\149\153q\"\179!5B_Q\173\028\243j6yn\129\2209\003<\168\156d)\169\247\212\199Q\031-\152"
-          0 4;
-        i1 Out_channel.close_noerr t;
-        i2 Out_channel.output_char t '\021';
-        i1 Out_channel.is_buffered t;
-        i2 Out_channel.output_byte t 2;
-        i2 Out_channel.seek t 83L;
-        i2 Out_channel.set_buffered t false)
+        Array.iter run tk1)
   in
 
   let dom2 =
     Domain.spawn (fun () ->
         Atomic.set wait false;
-        i2 Out_channel.set_binary_mode t false;
-        i2 Out_channel.output_char t '=';
-        i4 Out_channel.output_substring t "\153\002\213\144\240" 0 39;
-        i1 Out_channel.flush t;
-        i2 Out_channel.set_buffered t false;
-        i1 Out_channel.pos t;
-        i2 Out_channel.set_buffered t true;
-        i2 Out_channel.output_char t '\132')
+        Array.iter run tk2)
   in
 
   let _ = Domain.join dom1 in
@@ -49,9 +40,7 @@ let tester () =
   Sys.remove path
 
 let _ =
-  for i = 0 to 1000 do
-    for _j = 0 to 1000 do
-      tester () |> ignore
-    done;
-    Printf.printf "%d\n%!" (i + 1)
+  for i = 1 to 100000 do
+    List.iter tester log;
+    Printf.printf "%d\n%!" i
   done
