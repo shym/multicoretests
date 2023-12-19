aeec99b596b1d0e7f28666f0f3d4a0a1fc7b5664
Samuel Hym <samuel.hym@rustyne.lautre.net>
2023-12-19 15:37:23 +0100

    Specialise threadomain on one input

--- src/threadomain/threadomain.ml
+++ src/threadomain/threadomain.ml
@@ -1,31 +1,7 @@
-open QCheck2
-
 (* We mix domains and threads. We use the name _node_ for either a
    domain or a thread *)
 
-(** Generate a permutation of [0..sz-1] *)
-let permutation sz = Gen.shuffle_a (Array.init sz (fun x -> x))
-
-(** Generate a tree of size nodes
- The tree is represented as an array [a] of integers, [a.(i)] being
- the parent of node [i]. Node [0] is the root of the tree.
- *)
-let tree sz =
-  let open Gen in
-  let rec aux a i =
-    if i == 0
-    then ( a.(i) <- -1 ;
-           pure a )
-    else ( let* v = int_bound (i-1) in
-           a.(i) <- v ;
-           aux a (i-1) )
-  in aux (Array.make sz 0) (sz-1)
-
 type worktype = Burn | Tak of int
-
-let pp_worktype par fmt x =
-  let open Util.Pp in
-  match x with Burn -> cst0 "Burn" fmt | Tak x -> cst1 pp_int "Tak" par fmt x
 
 (** A test of spawn and join
 
@@ -47,58 +23,6 @@ type spawn_join = {
   domain_or:        bool array;
   workload:         worktype array
 }
-
-let pp_spawn_join par fmt
-    { spawn_tree; join_permutation; join_tree; domain_or; workload } =
-  let open Util.Pp in
-  pp_record par fmt
-    [
-      pp_field "spawn_tree" (pp_array pp_int) spawn_tree;
-      pp_field "join_permutation" (pp_array pp_int) join_permutation;
-      pp_field "join_tree" (pp_array pp_int) join_tree;
-      pp_field "domain_or" (pp_array pp_bool) domain_or;
-      pp_field "workload" (pp_array pp_worktype) workload;
-    ]
-
-let show_spawn_join = Util.Pp.to_show pp_spawn_join
-
-(* Ensure that any domain is higher up in the join tree than all its
-   threads, so that we cannot have a thread waiting on its domain even
-   indirectly *)
-let fix_permutation sz sj =
-  let swap arr i j =
-    let x = arr.(i) in
-    arr.(i) <- arr.(j) ;
-    arr.(j) <- x
-  in
-  let rec dom_of_thd i =
-    let candidate = sj.spawn_tree.(i) in
-    if candidate = -1 || sj.domain_or.(candidate)
-    then candidate
-    else dom_of_thd candidate
-  in
-  for i = 0 to sz-1 do
-    if not sj.domain_or.(i) then
-      let i' = sj.join_permutation.(i) in
-      let d = dom_of_thd i in
-      let d' = if d = -1 then d else sj.join_permutation.(d) in
-      if d' > i' then swap sj.join_permutation i d
-  done ;
-  sj
-
-let build_spawn_join sz spawn_tree join_permutation join_tree domain_or workload =
-  fix_permutation sz { spawn_tree; join_permutation; join_tree; domain_or; workload }
-
-let worktype =
-  let open Gen in
-  oneof [pure Burn; map (fun i -> Tak i) (int_bound 200)]
-
-let gen_spawn_join sz =
-  let open Gen in
-  build_spawn_join sz
-    <$> tree sz <*> permutation sz <*> tree sz
-    <*> array_size (pure sz) (frequencyl [(4, false); (1, true)])
-    <*> array_size (pure sz) worktype
 
 type handle =
   | NoHdl
@@ -177,19 +101,50 @@ let run_all_nodes sj =
   Array.for_all (fun h -> h = NoHdl) hdls.handles
    && Atomic.get global = sz
 
-let nb_nodes =
-  let max = if Sys.word_size == 64 then 100 else 16 in
-  Gen.int_range 2 max
-
-let main_test = Test.make ~name:"Mash up of threads and domains"
-                          ~count:500
-                          ~print:show_spawn_join
-                          (Gen.sized_size nb_nodes gen_spawn_join)
-                          run_all_nodes
-                          (* to debug deadlocks: *)
-                          (* (Util.fork_prop_with_timeout 1 run_all_nodes) *)
+let input = {
+  spawn_tree =
+  [|-1; 0; 0; 0; 0; 3; 2; 2; 0; 3; 7; 6; 9; 8; 9; 9; 3; 15; 0; 4;
+   12; 3; 7; 2; 14; 0; 3; 11; 21; 13; 10; 2; 3; 8; 31; 1; 24; 32; 19; 17;
+   8; 15; 7; 7; 23; 8; 45; 9; 22; 23; 0; 38; 5; 16; 11; 12; 16; 0; 17; 3;
+   4; 57; 22; 19; 37; 58; 0; 49; 47; 51; 51; 54; 61; 51; 50; 30; 31; 19; 36; 3;
+   51; 49; 67; 26; 22; 73; 73; 33; 56; 70; 43; 86; 85; 5; 67|];
+  join_permutation =
+  [|49; 69; 60; 26; 2; 41; 4; 30; 0; 6; 54; 16; 59; 84; 37; 72; 5; 8; 18; 33;
+    66; 45; 86; 15; 93; 81; 1; 7; 70; 58; 39; 11; 92; 83; 91; 55; 77; 68; 12; 38;
+    35; 24; 29; 87; 88; 3; 53; 31; 19; 57; 13; 20; 63; 48; 10; 22; 47; 25; 14; 73;
+    46; 80; 36; 67; 28; 89; 17; 79; 56; 65; 61; 32; 71; 94; 21; 52; 27; 44; 64; 43;
+    23; 34; 42; 85; 90; 50; 76; 9; 75; 51; 78; 82; 74; 40; 62|];
+  join_tree =
+  [|-1; 0; 1; 2; 2; 4; 1; 4; 0; 0; 7; 9; 5; 1; 6; 0; 6; 9; 5; 3;
+    9; 0; 1; 4; 19; 16; 24; 15; 9; 26; 0; 30; 15; 10; 21; 13; 24; 25; 16; 1;
+    31; 38; 21; 24; 12; 40; 44; 31; 13; 3; 27; 2; 31; 43; 34; 9; 53; 3; 10; 15;
+    24; 19; 44; 25; 1; 17; 55; 0; 28; 39; 55; 44; 0; 42; 25; 38; 11; 31; 49; 7;
+    65; 46; 31; 61; 26; 69; 39; 59; 0; 85; 2; 62; 30; 54; 22|];
+  domain_or =
+  [|false; false; false; true; false; false; false; false; true; true;
+    false; false; false; false; true; false; true; false; false; true;
+    false; false; false; false; false; false; true; false; true; false;
+    false; false; false; false; false; false; false; false; true; false;
+    false; false; false; false; true; false; false; false; false; false;
+    false; false; true; false; true; false; false; false; false; true;
+    false; false; true; false; false; false; false; false; false; false;
+    false; false; false; false; false; false; true; false; false; false;
+    false; false; false; false; true; true; true; false; false; false;
+    true; false; false; false; false|];
+  workload =
+  [|Tak 163; Burn; Burn; Burn; Tak 128; Tak 93; Burn; Tak 110; Burn; Burn;
+    Tak 11; Burn; Tak 9; Burn; Burn; Tak 173; Burn; Tak 136; Tak 186; Burn;
+    Tak 88; Burn; Tak 119; Tak 17; Burn; Tak 123; Burn; Burn; Burn; Tak 130;
+    Tak 94; Tak 125; Tak 160; Burn; Tak 56; Burn; Burn; Tak 185; Tak 138; Burn;
+    Burn; Tak 111; Burn; Tak 117; Tak 78; Tak 116; Burn; Burn; Tak 158; Tak 72;
+    Tak 51; Burn; Tak 45; Burn; Tak 27; Tak 153; Tak 55; Tak 167; Tak 37; Tak 191;
+    Tak 181; Burn; Tak 167; Burn; Tak 25; Tak 133; Tak 154; Burn; Burn; Burn;
+    Burn; Burn; Burn; Burn; Burn; Tak 101; Tak 17; Tak 190; Burn; Burn;
+    Burn; Tak 0; Burn; Tak 152; Tak 76; Burn; Burn; Burn; Tak 115; Burn;
+    Burn; Burn; Burn; Tak 87; Burn|]; }
 
 let _ =
-  QCheck_base_runner.run_tests_main [
-    main_test
-  ]
+  for i = 0 to 99999 do
+    Printf.printf "%d: %!" i;
+    Printf.printf "%b\n" (run_all_nodes input);
+  done
